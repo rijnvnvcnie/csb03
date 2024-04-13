b43beafc187a84237c53d3b62707aa0cf37b896a
rijnvnvcnie <leminhkhoi100808@gmail.com>
2024-04-13 19:27:23 +0700

    commit

---
+++ bttl/lesson2.py
@@ -0,0 +1,14 @@
+#b = input("your text: ")
+#txt = b
+#b = txt.count("we")
+#b = txt.strip()
+#b = txt.lower()
+#b = a[::-1]
+#print(b)
+a = input(int("a : "))
+b = input(int("b : "))
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+print(c)
---
+++ btvn/hw2.py
@@ -0,0 +1,11 @@
+#bk = float(input("bán kính: "))
+#a = 3.1416 * bk * bk
+#b = 2 * bk * 3.1416 
+#print(bk)
+#print(a)
+#print(b)
+
+c = float(input("cạnh: "))
+dc = (c**2 + c**2)**(1/2)
+print(c)
+print(dc)
